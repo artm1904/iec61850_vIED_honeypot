e882a38b61d6f22da816d2e91915cdc70ff351ca
Robin Dev <robin.dev@gmail.com>
2026-01-24 00:59:59 +0100

    updated hw_connector plugin

--- plugin_src/hw_connector/main.c
+++ plugin_src/hw_connector/main.c
@@ -8,6 +8,10 @@
 #include "iec61850_model_extensions.h"
 #include "inputs_api.h"
 #include "timestep_config.h"
+#include "LNParse.h"
+#include "XSWI.h"
+#include "TCTR.h"
+#include "TVTR.h"
 #include "config_parser.h"
 #include <stdio.h>
 #include <string.h>
@@ -25,12 +29,24 @@
 #define BUFFER_SIZE 4096
 #define LINE_BUFFER_SIZE 8192
 
+typedef struct sHwConfig
+{
+    int hwindex;
+    int socket;
+    void *inst;
+    struct sHwConfig *sibling;
+} HwConfig;
+
+HwConfig *HwConfigList_head = NULL;
+
 typedef struct {
     int sockfd;
     volatile int *shutdown_flag;
 } thread_args_t;
 
-static volatile int shutdown_flag = 0;
+int initSocket(const char *sock_path);
+int send_cmd(int sockfd, char * send_buf);
+
 // socket example code end
 
 // check ied-name, for config-entry
@@ -46,6 +62,29 @@ static volatile int shutdown_flag = 0;
 //     if cmd[0] == "SET" and len(cmd) == 3:    ch = int(cmd[1]) state_val = cmd[2]
 //   elif cmd[0] == "GET" and len(cmd) == 2:  ch = int(cmd[1])
 //   elif cmd[0] == "GETDATA":
+
+void XSWIcallback(void *inst, bool state)
+{
+    char buffer[64];
+    XSWI *instance = inst;
+    HwConfig *conf = instance->config;
+    sprintf(buffer, "SET %d %d",conf->hwindex, state);
+    send_cmd(conf->socket, buffer);
+}
+
+void * getInstViaHwIndex(int index)
+{
+    HwConfig *hwconf = HwConfigList_head;
+    while(hwconf)
+    {
+        if(hwconf->hwindex == index)
+        {
+            return hwconf->inst;
+        }
+        hwconf = hwconf->sibling;
+    }
+    return NULL;
+}
 
 int init(OpenServerInstance *srv)
 {
@@ -73,22 +112,81 @@ int init(OpenServerInstance *srv)
         return 1;
     }
 
-    printf(" Device: %s\n", section->section);
     const char *socket_path = config_get_value(section, "socket");
+    int socket = initSocket(socket_path);
+    if(socket == 0)
+    {
+        printf("ERROR: issue while opening socket: %s\n", socket_path);
+    }
 
     /* Iterate through all key-value pairs */
-    printf("\n === All settings for %s ===\n", model->name);
+    printf("\n settings for %s \n", section->section);
     for (int i = 0; i < section->entry_count; i++) {
-        printf("  %s = ", section->entries[i].key);
-        if (section->entries[i].value_count == 1) {
-            printf("%s\n", section->entries[i].values[0]);
-        } else {
-            printf("[");
-            for (int j = 0; j < section->entries[i].value_count; j++) {
-                printf("%s%s", section->entries[i].values[j],
-                       j < section->entries[i].value_count - 1 ? ", " : "");
+        //printf("  %s = ", section->entries[i].key);
+         // known keys
+        if(strcmp(section->entries[i].key,"socket") == 0)
+        {
+            printf("socket found\n");
+            continue;
+        }
+        //if not a known key, try it as an object-reference
+        LogicalNodeClass *ln = getLNClass(model, model_ex, section->entries[i].key);
+        if (ln == NULL)
+        {
+            printf("ERROR: could not parse or find an LN entry with key: %s\n", section->entries[i].key);
+            continue; //if not, give up
+        }
+        if(strcmp(ln->lnClass,"XSWI") == 0)
+        {
+            if (section->entries[i].value_count != 1) {
+                printf("ERROR: XSWI: incorrect value format. should only be 1 number\n");
+                continue;
             }
-            printf("]\n");
+            XSWI *item = ln->instance;
+            HwConfig *conf = (HwConfig *)malloc(sizeof(HwConfig));
+            conf->hwindex = (int)strtol(section->entries[i].values[0], NULL, 10);
+            conf->socket = socket;
+            conf->inst = item;
+            //place conf in linked list
+            conf->sibling = HwConfigList_head;
+            HwConfigList_head = conf;
+            //
+            item->config = conf;
+            setXSWI_Callback(item, XSWIcallback);
+            printf("XSWI: set callback and hw index to %d\n", conf->hwindex);
+        }
+        if(strcmp(ln->lnClass,"XCBR") == 0)
+        {
+            if (section->entries[i].value_count != 1) {
+                printf("ERROR: XCBR: incorrect value format. should only be 1 number\n");
+                continue;
+            }
+            XSWI *item = ln->instance;
+            HwConfig *conf = (HwConfig *)malloc(sizeof(HwConfig));
+            conf->hwindex = (int)strtol(section->entries[i].values[0], NULL, 10);
+            conf->socket = socket;
+            conf->inst = item;
+            //place conf in linked list
+            conf->sibling = HwConfigList_head;
+            HwConfigList_head = conf;
+            //
+            item->config = conf;
+            setXSWI_Callback(item, XSWIcallback);
+            printf("XCBR: set callback and hw index to %d\n", conf->hwindex);
+        }
+        if(strcmp(ln->lnClass,"TCTR") == 0)
+        {
+            TCTR *item = ln->instance;
+            if (section->entries[i].value_count == 1) {
+                printf("TCTR: %s\n", section->entries[i].values[0]);
+            }
+        }
+        if(strcmp(ln->lnClass,"TVTR") == 0)
+        {
+            TVTR *item = ln->instance;
+            if (section->entries[i].value_count == 1) {
+                printf("TVTR: %s\n", section->entries[i].values[0]);
+            }
         }
     }
 
@@ -100,19 +198,15 @@ int init(OpenServerInstance *srv)
 }
 
 
-
-void signal_handler(int signum) {
-    shutdown_flag = 1;
-}
-
 void *receiver_thread(void *arg) {
+    bool shutdown_flag = false;
     thread_args_t *args = (thread_args_t *)arg;
     int sockfd = args->sockfd;
     char buffer[BUFFER_SIZE];
     char line_buffer[LINE_BUFFER_SIZE] = {0};
     int line_pos = 0;
     
-    while (!shutdown_flag) {
+    while (open_server_running() && !shutdown_flag) {
         ssize_t n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
         
         if (n < 0) {
@@ -121,7 +215,7 @@ void *receiver_thread(void *arg) {
                 continue;
             }
             if (!shutdown_flag) {
-                fprintf(stderr, "\n[Receiver error: %s]\n", strerror(errno));
+                printf("\n[Receiver error: %s]\n", strerror(errno));
             }
             break;
         }
@@ -147,11 +241,56 @@ void *receiver_thread(void *arg) {
                     // Check if it's a broadcast event
                     if (strncmp(line, "EVENT ", 6) == 0) {
                         char *event_data = line + 6;
-                        printf("\n[BROADCAST] %s\n", event_data);
+                        //printf("\n[BROADCAST] %s\n", event_data);
+                        // Distribute event to XSWI/XCBR/TVTR/TCTR
+                        if(strncmp(event_data, "DATA", 4) == 0){
+                            printf("recv: %s\n", &event_data[5]); //Avalue,value,... Sbyte,byte,... A=analog value, S=shortcitcuit yes/no        A0,1,2,3,4,5,6,7,8,9,10,11 S01,02,03,04,05,06
+                        }
+                        if(strncmp(event_data, "IO", 2) == 0){
+                            char * endp;
+                            printf("recv: %s\n", &event_data[3]);//{channel} {position}
+                            int index = strtol(&event_data[3],&endp,10);
+                            // look up XSWI, based on index
+                            XSWI * instance = getInstViaHwIndex(index);
+                            if(instance != NULL)
+                            {                                
+                                if(index < 10)
+                                {
+                                    int state = strtol(endp,NULL,10);
+                                    if(state == 00)
+                                    {
+                                        XSWI_change_switch(instance, DBPOS_INTERMEDIATE_STATE);
+                                    }
+                                    if(state == 01)
+                                    {
+                                        XSWI_change_switch(instance, DBPOS_OFF);
+                                    }
+                                    if(state == 10)
+                                    {
+                                        XSWI_change_switch(instance, DBPOS_ON);
+                                    }
+                                }
+                                else
+                                {
+                                    if(endp[1] == 'F')
+                                    {
+                                        XSWI_change_switch(instance, DBPOS_OFF);
+                                    }
+                                    if(endp[1] == 'T')
+                                    {
+                                        XSWI_change_switch(instance, DBPOS_ON);
+                                    }
+                                }
+                            }
+                            else
+                            { 
+                                printf("ERROR: could not find XSWI instance for hwindex: %d\n", index);
+                            }
+                        }
                     } else {
-                        printf("\n[RESPONSE] %s\n", line);
+                        printf("\n[RESPONSE] %s\n NOT IMPLEMENTED YET\n", line);
+                        
                     }
-                    printf("> ");
                     fflush(stdout);
                 }
                 
@@ -163,23 +302,17 @@ void *receiver_thread(void *arg) {
             }
         }
     }
-    
+    close(sockfd);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <unix_socket_path>\n", argv[0]);
-        return 1;
-    }
-    
-    const char *sock_path = argv[1];
+int initSocket(const char *sock_path) {
     
     // Create socket
     int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("socket");
-        return 1;
+        return 0;
     }
     
     // Set receive timeout
@@ -189,7 +322,7 @@ int main(int argc, char *argv[]) {
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
         perror("setsockopt");
         close(sockfd);
-        return 1;
+        return 0;
     }
     
     // Connect to Unix socket
@@ -199,82 +332,33 @@ int main(int argc, char *argv[]) {
     strncpy(addr.sun_path, sock_path, sizeof(addr.sun_path) - 1);
     
     if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        fprintf(stderr, "Failed to connect to %s: %s\n", sock_path, strerror(errno));
+        printf("ERROR: Failed to connect to %s: %s\n", sock_path, strerror(errno));
         close(sockfd);
-        return 1;
+        return 0;
     }
     
-    printf("Connected to %s\n", sock_path);
-    printf("\nCommands:\n");
-    printf("  SET <channel> <value>    - Set channel state\n");
-    printf("  GET <channel>            - Get current event state for channel\n");
-    printf("  GETDATA                  - Get current data message\n");
-    printf("  quit                     - Exit program\n");
-    printf("\nBroadcast events will be displayed automatically as [BROADCAST]\n");
-    printf("------------------------------------------------------------\n");
-    
-    // Set up signal handler for Ctrl+C
-    signal(SIGINT, signal_handler);
+    //printf("Connected to %s\n", sock_path);
+    //printf("\nCommands:\n");
+    //printf("  SET <channel> <value>    - Set channel state\n");
+    //printf("  GET <channel>            - Get current event state for channel\n");
+    //printf("  GETDATA                  - Get current data message\n");
+    //printf("  quit                     - Exit program\n");
+    //printf("\nBroadcast events will be displayed automatically as [BROADCAST]\n");
+    //printf("------------------------------------------------------------\n");
     
     // Start receiver thread
-    pthread_t recv_tid;
-    thread_args_t thread_args = {sockfd, &shutdown_flag};
-    
-    if (pthread_create(&recv_tid, NULL, receiver_thread, &thread_args) != 0) {
-        perror("pthread_create");
-        close(sockfd);
-        return 1;
+    thread_args_t thread_args = {sockfd};
+    Thread thread = Thread_create((ThreadExecutionFunction)receiver_thread, &thread_args, true);
+    Thread_start(thread);
+        
+    return sockfd;
+}
+
+int send_cmd(int sockfd, char * send_buf)
+{
+    // Send command with newline
+    ssize_t sent = send(sockfd, send_buf, strlen(send_buf), 0);
+    if (sent < 0) {
+        printf("[Send error: %s]\n", strerror(errno));
     }
-    
-    // Main input loop
-    char cmd[1024];
-    while (!shutdown_flag) {
-        printf("> ");
-        fflush(stdout);
-        
-        if (fgets(cmd, sizeof(cmd), stdin) == NULL) {
-            break;
-        }
-        
-        // Remove newline
-        size_t len = strlen(cmd);
-        if (len > 0 && cmd[len - 1] == '\n') {
-            cmd[len - 1] = '\0';
-            len--;
-        }
-        
-        // Trim leading/trailing whitespace
-        char *trimmed = cmd;
-        while (*trimmed == ' ' || *trimmed == '\t') trimmed++;
-        
-        if (*trimmed == '\0') {
-            continue;
-        }
-        
-        // Check for quit command
-        if (strcasecmp(trimmed, "quit") == 0 || strcasecmp(trimmed, "exit") == 0) {
-            break;
-        }
-        
-        // Send command with newline
-        char send_buf[1026];
-        snprintf(send_buf, sizeof(send_buf), "%s\n", trimmed);
-        
-        ssize_t sent = send(sockfd, send_buf, strlen(send_buf), 0);
-        if (sent < 0) {
-            fprintf(stderr, "[Send error: %s]\n", strerror(errno));
-            break;
-        }
-    }
-    
-    printf("\n[Interrupted]\n");
-    shutdown_flag = 1;
-    
-    // Wait for receiver thread to finish
-    pthread_join(recv_tid, NULL);
-    
-    close(sockfd);
-    printf("Disconnected\n");
-    
-    return 0;
 }
